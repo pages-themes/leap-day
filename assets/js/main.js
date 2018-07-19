75a2356314b6fd2e83b85501c63c0d7d5afebc93
Jen Selby <jselby@nuevaschool.org>
2018-07-18 17:35:37 -0700

    Save text to make it easier to see what is used where

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,8 +14,10 @@ $(window).resize(sectionHeight);
 
 $(function() {
   $("section h1, section h2, section h3").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+    var headerName = $(this).text();
+    var headerId = headerName.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'');
+    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + headerId + "'>" + headerName + "</a></li>");
+    $(this).attr("id", headerId);
     $("nav ul li:first-child a").parent().addClass("active");
   });
 
