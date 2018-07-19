faa982b818c0e29975f05d4af7026c234e6a43b7
Jen Selby <jselby@nuevaschool.org>
2018-07-18 17:40:34 -0700

    Allow links to correctly jump to a different header

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ $(function() {
   $("section h1, section h2, section h3").each(function(){
     var headerName = $(this).text();
     var headerId = headerName.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'');
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + headerId + "'>" + headerName + "</a></li>");
+    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "' id='nav-" + headerId + "'><a href='#" + headerId + "'>" + headerName + "</a></li>");
     $(this).attr("id", headerId);
     $("nav ul li:first-child a").parent().addClass("active");
   });
@@ -26,6 +26,16 @@ $(function() {
     $("html, body").animate({scrollTop: position}, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
+    event.preventDefault();
+  });
+
+  $(".jump-to-header").click(function(event) {
+    var position = $($(this).attr("href")).offset().top - 190;
+    $("html, body").animate({scrollTop: position}, 400);
+    $("nav ul li.active").each(function() {
+      $(this).removeClass("active");
+    });
+    $('#nav-' + $(this).attr("href").substring(1)).addClass("active");
     event.preventDefault();
   });
 
