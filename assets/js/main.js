002abe13c62288265224cc2cc3cd69404e086945
AlexCatze <alexcatze@gmail.com>
2023-08-06 19:57:51 +0300

    fix links

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,16 +12,22 @@
 
 $(window).resize(sectionHeight);
 */
+
+function encodeEntry(entry)
+{
+	return encodeURIComponent(entry.toLowerCase())
+}
+
 $(function() {
   $("section h1, section h2, section h3").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + encodeEntry($(this).text()) + "'>" + $(this).text() + "</a></li>");
+    $(this).attr("id",encodeEntry($(this).text()));
     $("nav ul li:first-child a").parent().addClass("active");
   });
 
   $("nav ul li").on("click", "a", function(event) {
     var position = $($(this).attr("href")).offset().top - 190;
-    $("html, body").animate({scrollTop: position}, 400);
+    $("section").animate({scrollTop: position}, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
     event.preventDefault();
