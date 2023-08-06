9c608f3e84376360bb198d5d1666f233bc3486af
AlexCatze <alexcatze@gmail.com>
2023-08-06 21:46:19 +0300

    refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,3 @@
-/*var sectionHeight = function() {
-  var total    = $(window).height(),
-      $section = $('section').css('height','auto');
-
-  if ($section.outerHeight(true) < total) {
-    var margin = $section.outerHeight(true) - $section.height();
-    $section.height(total - margin - 20);
-  } else {
-    $section.css('height','auto');
-  }
-}
-
-$(window).resize(sectionHeight);
-*/
-
 function encodeEntry(entry)
 {
 	return encodeURIComponent(entry.toLowerCase())
