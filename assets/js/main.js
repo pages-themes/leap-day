e9950d9db8aa526b31186080980d664a7c4e8509
AlexCatze <alexcatze@gmail.com>
2023-08-07 20:02:06 +0300

    new scrollers

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,20 +5,20 @@ function encodeEntry(entry)
 
 $(function() {
   $("section h1, section h2, section h3").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + encodeEntry($(this).text()) + "'>" + $(this).text() + "</a></li>");
+    $("#sidemenu ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + encodeEntry($(this).text()) + "'>" + $(this).text() + "</a></li>");
     $(this).attr("id",encodeEntry($(this).text()));
-    $("nav ul li:first-child a").parent().addClass("active");
+    $("#sidemenu ul li:first-child a").parent().addClass("active");
   });
 
-  $("nav ul li").on("click", "a", function(event) {
+  $("#sidemenu ul li").on("click", "a", function(event) {
     var position = $($(this).attr("href")).offset().top - 190;
     $("section").animate({scrollTop: position}, 400);
-    $("nav ul li a").parent().removeClass("active");
+    $("#sidemenu ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
     event.preventDefault();
   });
 
-  sectionHeight();
-
-  $('img').on('load', sectionHeight);
+  new SimpleBar($('#sidemenu')[0], { autoHide: true });
+  new SimpleBar($('section')[0], { autoHide: true });
 });
+
